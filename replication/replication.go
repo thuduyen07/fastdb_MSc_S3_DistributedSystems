73054e3eedd822bd6131d44b2f1efa6c387313e2
Duyen Nguyen <thuduyen07@gmail.com>
2023-09-07 14:12:02 +0700

    config sharding using ReplicaNode

--- replication/replication.go
+++ replication/replication.go
@@ -17,7 +17,19 @@ import (
 
 // ReplicaNode represents a node for data replication.
 type ReplicaNode struct {
-	ID       int    // ID của node (có thể dùng để xác định node)
+	Id       int    // ID của node (có thể dùng để xác định node)
 	Address  string // Địa chỉ của node (ví dụ: IP:Port)
 	Status   string // Trạng thái của node (ví dụ: "Active", "Inactive")
+	HashId uint32
 }
+
+// NewReplicaNode tạo một đối tượng ReplicaNode mới với các thông tin cơ bản.
+func NewReplicaNode(Id int, Address string, Status string) *ReplicaNode {
+	return &ReplicaNode{
+		Id:      Id,
+		Address: Address,
+		Status:  Status,
+		HashId:  crc32.Checksum([]byte(id)),
+	}
+}
+
---
+++ replication/ring.go
@@ -0,0 +1,66 @@
+package fastdb
+
+/* ------------------------------- Imports --------------------------- */
+
+import (
+	"errors"
+	"fmt"
+	"strconv"
+	"sync"
+	"bufio"
+	"net"
+	"strings"
+	"sync"
+	"time"
+	"github.com/marcelloh/fastdb/persist"
+)
+
+/* ---------------------- Constants/Types/Variables ------------------ */
+
+// Ring is a network of distributed nodes.
+type Ring struct {
+	Nodes Nodes
+}
+
+// Nodes is an array of nodes.
+type Nodes []ReplicaNode
+
+// Initializes new distribute network of nodes or a ring.
+func NewRing() *Ring {
+	return &Ring{Nodes : Nodes{}}
+}
+
+func (r *Ring) AddNode(id string, address string, status string) {
+	node := NewReplicaNode(id, address, status )  
+	r.Nodes = append(r.Nodes, node)
+	sort.Sort(r.Nodes)
+}
+
+func (n Nodes) Len() int           { return len(n) }
+func (n Nodes) Less(i, j int) bool { return n[i].HashId < n[j].HashId }
+func (n Nodes) Swap(i, j int)      { n[i], n[j] = n[j], n[i] }
+
+// Removes node from the ring if it exists, else returns 
+// ErrNodeNotFound.
+func (r *Ring) RemoveNode(id string) error {
+    for i, node := range r.Nodes {
+        if node.Id == ID {
+            r.Nodes = append(r.Nodes[:i], r.Nodes[i+1:]...)
+            return nil
+        }
+    }
+    return errors.New("Node not found")
+}
+
+// Gets node which is mapped to the key. Return value is identifer
+// of the node given in `AddNode`.
+func (r *Ring) Get(key string) string {
+	searchfn := func(i int) bool {
+	  return r.Nodes[i].HashId >= crc32.Checksum([]byte(key))
+	}
+	i := sort.Search(r.Nodes.Len(), searchfn)
+	if i >= r.Nodes.Len() {
+	  i = 0
+	}
+	return r.Nodes[i].Id
+}
